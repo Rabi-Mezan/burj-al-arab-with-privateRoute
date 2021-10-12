fe018e7a70091683d9f7a7e4907715d9b6c701f5
Rabi-Mezan <ravimezan@gmail.com>
2021-10-12 18:37:21 +0600

    login using email password added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,44 @@
-import React, { useContext } from 'react';
+import userEvent from '@testing-library/user-event';
+import React, { useContext, useState } from 'react';
 import { Link, useLocation, useHistory } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 import useFirebase from '../../hooks/useFirebase';
 import './Login.css'
 
 
 const Login = () => {
-
     const location = useLocation();
     const history = useHistory();
     const redirectUrl = location.state?.from;
 
 
-    const { setUser, signInUsingGoogle } = useFirebase();
+    const { user, setName, email, password, setEmail, SetPassword, isLogin, setIslogin, createUser, userLogin, signInUsingGoogle } = useAuth();
+
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePassword = (e) => {
+        SetPassword(e.target.value);
+    }
+
+    const toggleCheckBox = (e) => {
+        setIslogin(e.target.checked);
+    }
+
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        isLogin ? userLogin(email, password) : createUser(email, password);
+    }
+
+
+
 
 
     const handleGoogleSignIn = () => {
@@ -26,26 +53,31 @@ const Login = () => {
     return (
         <div className='login'>
             <div>
-                <h3>Login Here</h3>
-                <form>
-                    <p>Email <br /><input type="email" name="" id="" placeholder='your email' /></p>
-                    <p>Password <br /> <input type="password" name="" id="" placeholder='your password' /></p>
+                <h3>Please {isLogin ? "Login" : "Register"} Here</h3>
+                <form onSubmit={handleRegister}>
+                    {!isLogin && <p>Name <br /><input onBlur={handleName} type="text" name="" placeholder='your name' required /></p>}
+                    <p>Email <br /><input onBlur={handleEmail} type="email" name="" placeholder='your email' required /></p>
+                    <p>Password <br /> <input onBlur={handlePassword} type="password" name="" placeholder='your password' required /></p>
+                    <span>Existing User ?  <input onChange={toggleCheckBox} type="checkbox" name="Existing User" id="" /></span>
                     <button
-                        type="submit">Login
+                        type="submit">{isLogin ? 'Login' : 'Register'}
                     </button>
                 </form>
-                <div className='register'>
-                    <h5>
-                        New User ?
-                        <small>
-                            <Link to='/register'>Create Account</Link></small>
-                        <br />
-                        <button onClick={handleGoogleSignIn}>
-                            <i class="fab fa-google-plus-g">
-                            </i>Google Sign In
-                        </button>
-                    </h5>
-                </div>
+                {
+                    !user.email &&
+                    <div className='register'>
+                        <h5>
+                            New User ?
+                            <small>
+                                <Link to='/register'>Create Account</Link></small>
+                            <br />
+                            <button onClick={handleGoogleSignIn}>
+                                <i className="fab fa-google-plus-g">
+                                </i>Google Sign In
+                            </button>
+                        </h5>
+                    </div>
+                }
             </div>
 
         </div>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,19 +1,52 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
 import { useEffect, useState } from "react";
 import initAuth from "../Firebase/init";
 
 initAuth();
 
 const useFirebase = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, SetPassword] = useState('');
+    const [isLogin, setIslogin] = useState(false);
     const [user, setUser] = useState({});
     const [error, setError] = useState('');
 
     const auth = getAuth();
     const googleProvider = new GoogleAuthProvider();
 
+    // create new user sign in using email password
+    const createUser = (email, password) => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                setUser(result.user);
+                console.log(result.user);
+                setUsername();
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+    }
 
 
-    // sign in
+
+    const userLogin = (email, password) => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                setUser(result.user);
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+    }
+
+    // update profile 
+    const setUsername = () => {
+        updateProfile(auth.currentUser, { displayName: name })
+            .then(result => { })
+    }
+
+    // sign in with google
     const signInUsingGoogle = () => {
         return signInWithPopup(auth, googleProvider)
     }
@@ -39,6 +72,16 @@ const useFirebase = () => {
     return {
         user,
         error,
+        name,
+        setName,
+        email,
+        setEmail,
+        password,
+        SetPassword,
+        isLogin,
+        setIslogin,
+        createUser,
+        userLogin,
         signInUsingGoogle,
         logOut
     }
